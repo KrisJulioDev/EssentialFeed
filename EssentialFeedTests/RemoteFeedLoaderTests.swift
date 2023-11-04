b61f0c60537a4dcee2f04c3b24d8b00bbb0ef522
Khristoffer Julio <khristoffer.julio@rsvp.com.au>
2023-11-04 15:39:07 +0800

    Move HTTPClient class to protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,20 +8,44 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load(from url: URL = URL(string: "any-url")!) {
+        client.get(from: url)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
  
     func test_init_doesNotRequestDataOnInit() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
