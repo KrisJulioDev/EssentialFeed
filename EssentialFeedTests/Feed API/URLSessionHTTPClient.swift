4d15a354002d419205bde1d88853971f2ce97926
Khristoffer Julio <khristoffer.julio@rsvp.com.au>
2023-11-05 15:35:57 +0800

    Make URLProtocolStub intercept every network request instead of URL-based

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeedTests
+//
+//  Created by Khristoffer Julio on 11/5/23.
+//
+ 
+import Foundation
+import EssentialFeed 
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    private struct UnexpectedValuesRepresentation: Error {}
+
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
