198fcb4414d27f3c21624d64556cbe760a551b1d
Khristoffer Julio <khristoffer.julio@rsvp.com.au>
2023-11-06 13:32:58 +0800

    Save command requests cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -5,11 +5,18 @@
 //  Created by Khristoffer Julio on 11/6/23.
 //
 
-import XCTest 
+import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    let store: FeedStore
+    
     init(store: FeedStore) {
-
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeedCallCount += 1
     }
 }
 
@@ -25,5 +32,23 @@ class CacheFeedUseCaseTests: XCTestCase {
 
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
-
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
 }
