7f7f32d51f7499342d565490b9acdf71198d9e2a
KrisJulioDev <krisgamedev@gmail.com>
2023-11-08 18:03:58 +0800

    Add more tests in coredatafeedstore

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,21 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Khristoffer Julio on 11/8/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+    func test_load_deliversNoItemsOnEmptyCache() {
+        
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() {
+        
+    }
+}
