a1ae69a3beb2c899419fb951c9ff03be01dfcdb0
KrisJulioDev <krisgamedev@gmail.com>
2023-11-09 18:47:41 +0800

    Rendering Logic/UX

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,28 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Khristoffer Julio on 11/9/23.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+	public let feedImageContainer = UIView()
+	public let feedImageView = UIImageView()
+	
+	private(set) public lazy var feedImageRetryButton: UIButton = {
+		let button = UIButton()
+		button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+		return button
+	}()
+	
+	var onRetry: (() -> Void)?
+	
+	@objc private func retryButtonTapped() {
+		onRetry?()
+	}
+}
