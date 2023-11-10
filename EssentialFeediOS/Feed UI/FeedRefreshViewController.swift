c82fac4583d5cd7e6dd57adb28aa8e1195f25f6d
KrisJulioDev <krisgamedev@gmail.com>
2023-11-10 11:05:31 +0800

    Move FeedImageCellController and its image loading logic to separate file

---
+++ EssentialFeediOS/Feed UI/FeedRefreshViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedRefreshViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Khristoffer Julio on 11/10/23.
+//
+
+import UIKit
+import EssentialFeed
+
+final class RefreshViewController: NSObject {
+    private(set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
