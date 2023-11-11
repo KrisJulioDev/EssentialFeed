1bbf3d0cffbb62e51e8f96c9eaeccb5c022342e3
KrisJulioDev <krisgamedev@gmail.com>
2023-11-11 15:26:03 +0800

    Replace FeedPresenter parameter by loadFeed function to remove reference

--- EssentialFeediOS/Feed UI/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/FeedUIComposer.swift
@@ -13,7 +13,7 @@ public final class FeedUIComposer {
     
     public static func feedComposeWith(_ feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let feedPresenter = FeedPresenter(feedLoader: feedLoader)
-        let refreshController = RefreshViewController(presenter: feedPresenter)
+        let refreshController = RefreshViewController(loadFeed: feedPresenter.loadFeed)
         let feedController = FeedViewController(refreshController: refreshController)
         feedPresenter.feedLoadingView = WeakVirtualProxy(refreshController)
         feedPresenter.feedView = FeedAdapter(controller: feedController, loader: imageLoader)
