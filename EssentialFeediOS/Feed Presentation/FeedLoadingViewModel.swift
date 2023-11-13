eba22a58ba5b02fa0b7abd1611a037639c56f5e4
KrisJulioDev <krisgamedev@gmail.com>
2023-11-13 14:47:52 +0800

    Move FeedLoading and FeedViewModel to its own class

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Khristoffer Julio on 11/13/23.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Khristoffer Julio on 11/13/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
