615cb3391b07affad396ee2e04ed1ee31c80093d
KrisJulioDev <krisgamedev@gmail.com>
2023-11-15 13:07:25 +0800

    Add single empty iOS application project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Khristoffer Julio on 11/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
