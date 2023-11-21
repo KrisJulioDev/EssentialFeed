fd66528c26cea92a7db04ae2dfc8b95f8da061cd
Kris Julio <khristoffer.julio@gmail.com>
2023-11-21 11:42:50 +0800

    Change LoadImageCommentsFromRemoteUseCaseTests to  ImageCommentsMapperTests to test generic imagecomments mapper strategy

--- EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
+++ EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
@@ -1,5 +1,5 @@
 //
-//  LoadImageCommentsFromRemoteUseCaseTests.swift
+//  ImageCommentsMapperTests.swift
 //  EssentialFeedTests
 //
 //  Created by Khristoffer Julio on 11/20/23.
@@ -8,41 +8,58 @@
 import XCTest
 import EssentialFeed
 
-final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
-    func test_load_deliversErrorOnNon2xxHTTPResponse() {
-        let (sut, client) = makeSUT()
+final class ImageCommentsMapperTests: XCTestCase {
+    func test_map_throwsErrorOnNon2xxHTTPResponse() throws {
+        let statusCodes = [199, 150, 300, 400, 500]
+        let json = makeItemsJSON([])
         
-        let statusCodes = [199, 150, 300, 400, 500].enumerated()
-        statusCodes.forEach { (index, code) in
-            expect(sut, toCompleteWith: failure(RemoteImageCommentsLoader.Error.invalidData), when: {
-                let json = makeItemsJSON([])
-                client.complete(withStatusCode: code, data: json, at: index)
-            })
+        try statusCodes.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            )
         }
     }
     
-    func test_load_deliversErrorOn2xxHTTPResponseWithInvalidJSON() {
-        let (sut, client) = makeSUT()
+    func test_map_deliversErrorOn2xxHTTPResponseWithInvalidJSON() throws {
+        let statusCodes = [200, 201, 250, 280, 299]
+        let invalidJSON = Data("invalid json".utf8)
         
-        let statusCodes = [200, 201, 250, 280, 299].enumerated()
-        
-        statusCodes.forEach { (index, code) in
-            expect(sut, toCompleteWith: failure(RemoteImageCommentsLoader.Error.invalidData), when: {
-                let invalidJSON = Data("invalid json".utf8)
-                client.complete(withStatusCode: code, data: invalidJSON, at: index)
-            })
+        try statusCodes.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+            )
         }
     }
      
-    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
-        let (sut, client) = makeSUT()
-        let statusCodes = [200, 201, 250, 280, 299].enumerated()
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
+        let emptyListJSON = makeItemsJSON([])
+        let statusCodes = [200, 201, 250, 280, 299]
         
-        statusCodes.forEach { (index, code) in
-            expect(sut, toCompleteWith: .success([])) {
-                let emptyListJSON = makeItemsJSON([])
-                client.complete(withStatusCode: code, data: emptyListJSON, at: index)
-            }
+        try statusCodes.forEach { code in
+            let result = try ImageCommentsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: code))
+            XCTAssertEqual(result, [])
+        }
+    }
+     
+    func test_load_deliversItemsOn200HTTPResponseWithJSONItems() throws {
+        let item1 = makeItem(
+            id: UUID(),
+            message: "a message",
+            createdAt: (Date(timeIntervalSince1970: 1598627222), "2020-08-28T15:07:02+00:00"),
+            username: "a username")
+        
+        let item2 = makeItem(
+            id: UUID(),
+            message: "another message",
+            createdAt: (Date(timeIntervalSince1970: 1577881882), "2020-01-01T12:31:22+00:00"),
+            username: "another username")
+        
+        let statusCodes = [200, 201, 250, 280, 299]
+        let json = makeItemsJSON([item1.json, item2.json])
+
+        try statusCodes.forEach { code in
+            let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            XCTAssertEqual(result, [item1.model, item2.model])
         }
     }
     
